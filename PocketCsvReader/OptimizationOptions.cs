db5f2ea6b656034bbb484af60d83b0fd044a508d
Cédric L. Charlier <seddryck@gmail.com>
2024-11-10 22:28:00 +0100

    feat: configure some potential parser optimizations (#19)

---
+++ PocketCsvReader/OptimizationOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketCsvReader;
+public record OptimizationOptions
+(
+    bool NoTextQualifier = false,
+    bool UnescapeChars = true,
+    bool HandleSpecialValues = true,
+    bool RowCountAtStart = false
+) { }
