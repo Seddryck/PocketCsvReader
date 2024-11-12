f07d4efd9722559d40b20f63faa2444f7f485c29
Cédric L. Charlier <seddryck@gmail.com>
2024-11-12 08:50:11 +0100

    ci: versions are specified into assembly info

--- PocketCsvReader/Properties/ProjectAssemblyInfo.cs
+++ PocketCsvReader/Properties/ProjectAssemblyInfo.cs
@@ -2,12 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyDescription("PocketCsvReader is a lightweight library dedicated to the parsing of delimited flat file such as CSV or TSV files. The main function is to read the content of the file and load it into a DataTable.")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("622d8798-d870-4a3b-bbb8-87297b7085bf")]
 
 [assembly: InternalsVisibleTo("PocketCsvReader.Testing")]
