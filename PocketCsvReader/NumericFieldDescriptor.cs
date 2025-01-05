6e044251bb9b25ccb1aab7087dcbb040514f2da0
Cédric L. Charlier <seddryck@gmail.com>
2025-01-05 19:13:19 +0100

    feat: parse with custom decimal and group chars for numeric values

---
+++ PocketCsvReader/NumericFieldDescriptor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PocketCsvReader.Configuration;
+
+namespace PocketCsvReader;
+public record NumericFieldDescriptor 
+(
+    Type RuntimeType
+    , string? Name = null
+    , string? Format = null
+    , char ? DecimalChar = null
+    , char ? GroupChar = null
+) : FieldDescriptor(RuntimeType, Name, Format)
+{ }
