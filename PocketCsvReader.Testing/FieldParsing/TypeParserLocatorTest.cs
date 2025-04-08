6c6576fc0f11bb294d2cfd220a142c4ead280a87
Cédric L. Charlier <seddryck@gmail.com>
2025-04-08 09:21:47 +0200

    fix: parse numeric from temporal fields (#147)

--- PocketCsvReader.Testing/FieldParsing/TypeParserLocatorTest.cs
+++ PocketCsvReader.Testing/FieldParsing/TypeParserLocatorTest.cs
@@ -29,4 +29,54 @@ public class TypeParserLocatorTest
         var value = func.Invoke(input);
         Assert.That(value, Is.EqualTo(new Chrononuensis.YearQuarter(2025, 1)));
     }
+
+    [Test]
+    [TestCase("10-12-2025")]
+    public void Locate_DateTime_FindIt(string input)
+    {
+        var locator = new TypeParserLocator<DateTime>();
+        var func = locator.Locate(["dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None]);
+        var value = func.Invoke(input);
+        Assert.That(value, Is.EqualTo(new DateTime(2025, 12, 10)));
+    }
+
+    [Test]
+    [TestCase("10-12-2025")]
+    public void Locate_DateOnly_FindIt(string input)
+    {
+        var locator = new TypeParserLocator<DateOnly>();
+        var func = locator.Locate(["dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None]);
+        var value = func.Invoke(input);
+        Assert.That(value, Is.EqualTo(new DateOnly(2025, 12, 10)));
+    }
+
+    [Test]
+    [TestCase("165")]
+    public void Locate_Int32_FindIt(string input)
+    {
+        var locator = new TypeParserLocator<int>();
+        var func = locator.Locate([NumberStyles.Integer, CultureInfo.InvariantCulture]);
+        var value = func.Invoke(input);
+        Assert.That(value, Is.EqualTo(165));
+    }
+
+    [Test]
+    [TestCase("165")]
+    public void Locate_Int32AsTemporal_FindIt(string input)
+    {
+        var locator = new TypeParserLocator<int>();
+        var func = locator.Locate(["dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None]);
+        var value = func.Invoke(input);
+        Assert.That(value, Is.EqualTo(165));
+    }
+
+    [Test]
+    [TestCase("165")]
+    public void Locate_Int16_FindIt(string input)
+    {
+        var locator = new TypeParserLocator<short>();
+        var func = locator.Locate([NumberStyles.Integer, CultureInfo.InvariantCulture]);
+        var value = func.Invoke(input);
+        Assert.That(value, Is.EqualTo((short)165));
+    }
 }
